f819a40de050885469abd32cd80256b195f69dc5
Clark Mulvey <clarkgmulvey2@gmail.com>
2021-01-17 22:56:53 -0700

    Week 2 notes JS

--- week2/scripts/week2.js
+++ week2/scripts/week2.js
@@ -1,7 +1,52 @@
+const templateLiteralDiv = document.querySelector("#templateLiteral");
+templateLiteralDiv.innerHTML = `This is a template literal using backticks
+
+I am putting new lines in the string declaration
+
+As you can see the render looks like this`;
+
+console.log(`This is a template literal using backticks
+
+I am putting new lines in the string declaration
+
+As you can see the render looks like this`);
+
 let student = {
     firstName: 'Clark',
     lastName: 'Mulvey',
     iNumber: 123145621
 }
 
-console.dir(student["iNumber"]);
+//console.dir(student["iNumber"]);
+
+//function declaration
+function save1() {
+    const name = document.querySelector('#name').value;
+    console.log(name);
+
+    const result = document.querySelector('#result');
+    result.innerHTML = name;
+
+
+
+
+}
+
+// function expression
+const save2 = function() {
+    console.log('save2');
+}
+
+// arrow function
+const save3 = () => {
+    console.log('save3');
+}
+
+document.querySelector('button').addEventListener('click', save1);
+
+
+//############# Quiz Ninja Project Chapter 2 #############
+const question = "What is Superman's real name?";
+const answer = prompt(question);
+alert(`You answered ${answer}`);
+
---
+++ week2/teamActivity/teamActivity.js
@@ -0,0 +1,6 @@
+const readAndWrite = () => {
+    const num1 = document.querySelector('#num1').value;
+    document.querySelector('#div1').innerHTML = num1;
+}
+
+document.querySelector
