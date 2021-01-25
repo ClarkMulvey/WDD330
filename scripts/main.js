c35356adf2833b467b6bee4f06e0f75ed67934d6
Clark Mulvey <clarkgmulvey2@gmail.com>
2021-01-24 22:14:26 -0700

    Changed main.js a tag hrefs

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,15 @@ let ol = document.querySelector('ol');
 const links =  [
     {
         label: "Week 1",
-        url: "../week1/index.html"
+        url: "week1/index.html"
     },
     {
         label: "Week 2",
-        url: "../week2/index.html"
+        url: "week2/index.html"
     },
     {
         label: "Week 3",
-        url: "../week3/index.html"
+        url: "week3/index.html"
     }
 ]
 
